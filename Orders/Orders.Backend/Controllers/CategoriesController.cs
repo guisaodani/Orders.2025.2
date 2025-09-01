ca9f693a7fe1bdc68cf6d7651811db4a3c2097b0
guisaodani <guisaodani@gmail.com>
2025-08-31 23:17:04 -0500

    complete generic controller

---
+++ Orders/Orders.Backend/Controllers/CategoriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.Backend.UnitsOfWork.Interfaces;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CountegoriesController : GenericController<Category>
+{
+    public CountegoriesController(IGenericUnitOfWork<Category> unitOfWork) : base(unitOfWork)
+    {
+    }
+}
